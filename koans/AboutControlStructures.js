39c2ffb858dce944dedc51d3fe898aca6d9157d3
Alex <alexkononenko6@protonmail.com>
2022-07-08 15:39:31 +0200

    08/07/2022

--- koans/AboutControlStructures.js
+++ koans/AboutControlStructures.js
@@ -38,10 +38,10 @@ describe("About Control Structures", () => {
 
   it("ternary operator", function() {
     var fruit = true ? "apple" : "orange";
-    expect(FILL_ME_IN).withContext('what is the value of fruit?').toEqual(fruit);
+    expect("apple").withContext('what is the value of fruit?').toEqual(fruit);
 
     fruit = false ? "apple" : "orange";
-    expect(FILL_ME_IN).withContext('now what is the value of fruit?').toEqual(fruit);
+    expect("orange").withContext('now what is the value of fruit?').toEqual(fruit);
   });
 
   it("switch", function() {
@@ -54,7 +54,7 @@ describe("About Control Structures", () => {
             result = 2;
             break;
     }
-    expect(FILL_ME_IN).withContext('what is the value of result?').toEqual(result);
+    expect(2).withContext('what is the value of result?').toEqual(result);
   });
 
   it("switch default case", function() {
@@ -70,12 +70,12 @@ describe("About Control Structures", () => {
             result = "Merry";
             break;
     }
-    expect(FILL_ME_IN).withContext('what is the value of result?').toEqual(result);
+    expect("Merry").withContext('what is the value of result?').toEqual(result);
   });
 
   it("null coalescing", function() {
     var result = null || "a value";
-    expect(FILL_ME_IN).withContext('what is the value of result?').toEqual(result);
+    expect("a value").withContext('what is the value of result?').toEqual(result);
   });
 
 });
--- koans/AboutNumbers.js
+++ koans/AboutNumbers.js
@@ -4,14 +4,14 @@ describe("About Numbers", () => {
     var typeOfIntegers = typeof(6);
     var typeOfFloats = typeof(3.14159);
 
-    expect(FILL_ME_IN).withContext('are ints and floats the same type?').toEqual(typeOfIntegers === typeOfFloats);
-    expect(FILL_ME_IN).withContext('what is the javascript numeric type?').toEqual(typeOfIntegers);
-    expect(FILL_ME_IN).withContext('what is a integer number equivalent to 1.0?').toEqual(1.0);
+    expect(true).withContext('are ints and floats the same type?').toEqual(typeOfIntegers === typeOfFloats);
+    expect("number").withContext('what is the javascript numeric type?').toEqual(typeOfIntegers);
+    expect(1).withContext('what is a integer number equivalent to 1.0?').toEqual(1.0);
   });
 
   it("NaN", function() {
     var resultOfFailedOperations = 7/'apple';
-    expect(FILL_ME_IN).withContext('what will satisfy the equals assertion?').toEqual(isNaN(resultOfFailedOperations));
-    expect(FILL_ME_IN).withContext('is NaN == NaN?').toEqual(resultOfFailedOperations == NaN);
+    expect(true).withContext('what will satisfy the equals assertion?').toEqual(isNaN(resultOfFailedOperations));
+    expect(false).withContext('is NaN == NaN?').toEqual(resultOfFailedOperations == NaN);
   });
 });
